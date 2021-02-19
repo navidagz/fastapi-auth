61c77a5c5aedd09b18544d10266cb8d228e9ad92
navidagz <navidagz76@gmail.com>
2021-02-19 15:06:21 +0330

    refactor

--- config/fastapi_users.py
+++ config/fastapi_users.py
@@ -1,8 +1,11 @@
+import types
+
 from fastapi_users import FastAPIUsers
 
 from config.jwt import jwt_authentication
 from db.init import user_db
 from db.schema import User, UserDB, UserUpdate, UserCreate
+from routers.v1.auth import get_refresh_token_router, get_verify_account_router
 
 fastapi_users = FastAPIUsers(
     user_db,
@@ -12,3 +15,5 @@ fastapi_users = FastAPIUsers(
     UserUpdate,
     UserDB,
 )
+fastapi_users.get_refresh_token = types.MethodType(get_refresh_token_router, fastapi_users)
+fastapi_users.get_verify_account = types.MethodType(get_verify_account_router, fastapi_users)
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import types
-
 from fastapi import FastAPI
 
 from config.after_actions import after_verification, after_verification_request
@@ -7,7 +5,6 @@ from config.fastapi_users import fastapi_users
 from config.jwt import jwt_authentication
 from config.route_prefix import AUTH_V1_PREFIX, AUTH_TAGS, USERS_V1_PREFIX, USERS_TAGS
 from config.settings import settings
-from routers.v1.auth import get_refresh_token_router, get_verify_account_router
 
 app = FastAPI()
 
@@ -37,7 +34,6 @@ app.include_router(
 )
 
 # Verify Account
-fastapi_users.get_verify_account = types.MethodType(get_verify_account_router, fastapi_users)
 app.include_router(
     fastapi_users.get_verify_account(settings.secret, after_verification=after_verification),
     prefix=AUTH_V1_PREFIX,
@@ -45,7 +41,6 @@ app.include_router(
 )
 
 # Refresh token
-fastapi_users.get_refresh_token = types.MethodType(get_refresh_token_router, fastapi_users)
 app.include_router(
     fastapi_users.get_refresh_token(jwt_authentication, requires_verification=True),
     prefix=AUTH_V1_PREFIX,
